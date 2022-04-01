1d4bef0489aa04c6a073af575fbaf39a45768dc4
jdaeth274 <joshdaeth@yahoo.co.uk>
2022-04-01 19:05:28 +0100

    adding in GTR poisson script and varying aln length bash

---
+++ bash/simul_script_poisson_JC_VARYING_LENGTH.sh
@@ -0,0 +1,32 @@
+#! /usr/local/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+PARENT="$(dirname "$BASE_DIR")"
+PERLDIR="${PARENT}/perl/"
+
+BRANCH_RATE=$1
+REC_RATE=$2
+DONOR_ALN=$3
+PHAGE_ALN=$4
+NUM_SEQS=$5
+
+if [ $# -ne 5 ]
+then
+	echo "Incorrect number of argument need 5 have $# "
+	echo "   "
+	echo "bash <branch_rate> <rec_rate> <donor_aln> <phage_aln> <num_seqs>"
+	echo ""
+	exit 1
+fi
+
+dir="sim-branch-${BRANCH_RATE}-rec-${REC_RATE}"
+if [ -d $dir ]
+then
+	rm -r $dir
+	mkdir $dir
+else
+	mkdir $dir
+fi
+cd $dir
+perl "${PERLDIR}generate_taxa_poisson_rec.pl" -a $DONOR_ALN \
+-o $dir -n $NUM_SEQS -m $PHAGE_ALN -i 0 -b $BRANCH_RATE -r $REC_RATE
