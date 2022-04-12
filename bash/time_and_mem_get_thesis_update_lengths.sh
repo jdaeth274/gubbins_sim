85ee324c61a1ea3252566ce5657497fb995738c3
jdaeth274 <joshdaeth@yahoo.co.uk>
2022-04-12 09:36:46 +0100

    adding in various length pbs scripts

---
+++ bash/time_and_mem_get_thesis_update_lengths.sh
@@ -0,0 +1,56 @@
+#! /bin/bash/
+
+#set -e 
+## Point  this to the parent directory of the rep runs  
+## Use the R script to get the memory usage and run time for each model. 
+
+#GUBBINS_LOG=$1
+OUT_FILE=$1
+MODEL_FILE=$2
+MODEL_DIR=$3
+MODEL_NUM=1
+
+if [ $# -ne 3 ]
+then
+echo "Need three arguments see usage below:"
+echo ""
+echo "bash time_and_mem_get.sh <out_name> <list_of_model_file> <results_dir>"
+echo ""
+exit
+fi
+
+if [ -f $OUT_FILE ]
+then
+echo "Output file already exists, overwriting this one completely"
+rm $OUT_FILE
+fi
+
+##Loop through the rep runs 
+for K in {1..10};
+do 
+cd "rep_${K}_runs"
+## Now we need to go through each of the sim conditions 
+while read seq <&3
+do
+cd "sim-branch-0.1-rec-0.5-length-${seq}"
+cat $MODEL_FILE | while read line
+do 
+echo "$MODEL_NUM"
+TIM_FILE="${line}_time.txt"
+echo $TIM_FILE
+if [ -f $TIM_FILE ]
+then 
+cat $TIM_FILE >> $OUT_FILE
+CURRENT_GUBB="${line}/gubbins_log"
+echo $CURRENT_GUBB
+grep "(wall clock)" $CURRENT_GUBB >> $OUT_FILE || echo "No time data for this run"
+grep "Maximum resident set size" $CURRENT_GUBB >> $OUT_FILE || echo "No memory usage data for this run"
+fi
+MODEL_NUM=$(( MODEL_NUM + 1 ))
+done
+cd ../
+done 3< ~/gubbins_testing/seq_lengths.txt
+cd ../
+done 
+
+
