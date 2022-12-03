f78d097db3b1e8bdf9fc0aec8b311db78690a81f
jdaeth274 <joshdaeth@yahoo.co.uk>
2022-12-03 12:59:14 +0000

    moving the 20k pareto files

--- bash/pareto_mover_20k.sh
+++ bash/pareto_mover_20k.sh
@@ -6,7 +6,8 @@ cd ~/../ephemeral/gubbins_testing/pareto_20k_runs
 
 mkdir -p pareto_20k_gubbins_trees
 mkdir -p pareto_20k_gubbins_csvs
-
+mkdir -p pareto_20k_gubbins_gffs
+mkdir -p pareto_20k_gubbins_embls
 
 
 for K in {1..10}
@@ -20,8 +21,10 @@ do
             cd $model 
             SIM=$(basename $line)
 	    echo $PWD
-            cp *per_branch* "../../../pareto_20k_gubbins_csvs/${model}-rep-${K}-${SIM}.per_branch_statistics.csv"
-            cp *node_labelled* "../../../pareto_20k_gubbins_trees/${model}-rep-${K}-${SIM}.node_labelled.final_tree.tre" 
+            cp *per_branch* "../../../pareto_20k_gubbins_csvs/${model}-rep-${K}-${SIM}-JC-JC.per_branch_statistics.csv"
+            cp *node_labelled* "../../../pareto_20k_gubbins_trees/${model}-rep-${K}-${SIM}-JC-JC.node_labelled.final_tree.tre" 
+	    cp *recombination_predictions.gff "../../../pareto_20k_gubbins_gffs/${model}-rep-${K}-${SIM}-JC-JC.recombination_predictions.gff"
+	    cp *embl_csv.csv ../../../pareto_20k_gubbins_embls
             printf "Done on ${K}-${SIM}-${model}\n"
             cd ../
         done 3< ~/gubbins_testing/gubbins_3.3_simple_complex.txt
