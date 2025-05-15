12ad9fac6508e4978ede88ebe856f2cf835453b5
jdaeth274 <joshdaeth@yahoo.co.uk>
2025-05-15 20:44:43 +0100

    allowing for more flexibility in pareto mover

--- bash/pareto_mover.sh
+++ bash/pareto_mover.sh
@@ -13,7 +13,7 @@ mkdir -p pareto_0_gubbins_gff
 for K in {1..10}
 do
     cd "rep_${K}_runs"
-    ls -d * > dir_list.txt
+    ls -d */ > dir_list.txt
     cat dir_list.txt | while read line;
     do 
         cd $line
